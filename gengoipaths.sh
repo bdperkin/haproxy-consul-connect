0d85d84e9bbafd1819f6a0f4968f506b8e195e81
Brandon Perkins <bperkins@redhat.com>
2021-06-01 12:23:30 -0400

    rewrite of ipath generation

--- gengoipaths.sh
+++ gengoipaths.sh
@@ -1,59 +1,106 @@
 #! /bin/bash
 
-echo "#IPATH,FORGE,SUBDIR,ALTIPATHS,VERSION,TAG,COMMIT,SRCTGZ,DSTTGZ" > goipaths.txt
+echo "#IPATH,FORGE,SUBDIR,ALTIPATHS,VERSION,TAG,COMMIT,SRCTGZ,DSTTGZ,TYPE" > goipaths.txt
 
-declare -A FPATHOVERRIDE
+for IPATH in $(cat packages.txt); do
+    echo "========== BEGIN ${IPATH} =========="
+    echo "IPATH: ${IPATH}"
 
-FPATHOVERRIDE['code.google.com/p/go-uuid/uuid']='https://github.com/pborman/uuid'
+    NAME=$(basename ${IPATH})
+    echo "NAME: ${NAME}"
 
-for PKG in $(cat packages.txt); do
-    echo ${PKG}
-    TMPFILE=$(mktemp)
-    curl -s https://pkg.go.dev/${PKG} > ${TMPFILE}
-    FPATH=$(grep -A 7 Repository ${TMPFILE} | grep -P -o '<a href=".*" ' | cut -d\" -f2 | grep -P -o "https:\/\/.*")
+    PKGGODEVFILE=$(mktemp)
+    URL="https://pkg.go.dev/${IPATH}"
+    echo "URL: ${URL}"
+
+    curl -s ${URL} > ${PKGGODEVFILE}
+    FPATH=$(grep -A 7 Repository ${PKGGODEVFILE} | grep -P -o '<a href=".*" ' | cut -d\" -f2 | grep -P -o "https:\/\/.*")
     if [ "${FPATH}" == "" ]; then
-        FPATH=${FPATHOVERRIDE[${PKG}]}
+        echo "Cannot find FPATH"
+        exit 1
     fi
-    if [ "${FPATH}" == "" ]; then
-        echo "Cannot determine FPATH for ${PKG}"
+    echo "FPATH: ${FPATH}"
+
+    DVERSION=$(grep -P 'data-version=".*"' ${PKGGODEVFILE} | head -1 | cut -d\" -f2)
+    DMODPATH=$(grep -P 'data-mpath=".*"' ${PKGGODEVFILE} | head -1 | cut -d\" -f2)
+    DPKGPATH=$(grep -P 'data-ppath=".*"' ${PKGGODEVFILE} | head -1 | cut -d\" -f2)
+    DTYPE=$(grep -P 'data-pagetype=".*"' ${PKGGODEVFILE} | head -1 | cut -d\" -f2)
+    for D in DVERSION DMODPATH DPKGPATH DTYPE; do
+        echo "${D}: ${!D}"
+    done
+
+    grep "^${DMODPATH}," goipaths.txt
+    if [ $? -eq 0 ]; then
+        echo -n "EXISTS: "
+        grep "^${DMODPATH}," goipaths.txt
         continue
     fi
-    TAG=$(grep -P 'data-version=".*"' ${TMPFILE} | head -1 | cut -d\" -f2)
-    echo "${TAG}" | grep '-'
-    if [ $? -eq 0 -o "${TAG}" == "" ]; then
-        TAG=$(git ls-remote ${FPATH}.git | grep -v '-' | grep -v '\^{}$' | tail -1 | rev | cut -d\/ -f1 | rev)
-    fi
-    VERSION=""
-    COMMIT=""
-    echo ${TAG} | grep '^v'
-    if [ $? -eq 0 ]; then
-        VERSION=$(echo -n ${TAG} | sed -e 's/^v//g')
-            TAG=""
-    else
-        COMMIT=$(git ls-remote ${FPATH}.git refs/tags/${TAG} | awk '{print $1}')
-    fi
-    echo ${TAG} | grep '\.'
-    if [ $? -ne 0 -a "${TAG}" != "" ]; then
-        COMMIT=$(git ls-remote ${FPATH}.git HEAD | awk '{print $1}')
-        TAG=""
-    fi
-    MPATH=$(grep -P 'data-mpath=".*"' ${TMPFILE} | head -1 | cut -d\" -f2)
-    if [ "${MPATH}" == "" ]; then
-        MPATH=${PKG}
-    fi
-    PPATH=$(grep -P 'data-ppath=".*"' ${TMPFILE} | head -1 | cut -d\" -f2)
-    if [ "${PPATH}" == "" ]; then
-        PPATH=${PKG}
-    fi
-    echo "${VERSION} - ${MPATH} - ${PPATH} - ${FPATH}"
-    grep "^${MPATH}," goipaths.txt
-    if [ $? -ne 0 ]; then
-        echo "${VERSION}" | grep '-'
-        if [ $? -eq 0 ]; then
-            VERSION=$(git ls-remote ${FPATH}.git | grep -v '-' | grep -v '\^{}$' | tail -1 | rev | cut -d\/ -f1 | rev | sed -e 's/^v//g')
-        fi
-        echo "${MPATH},${FPATH},,,${VERSION},${TAG},${COMMIT},," >> goipaths.txt
-    fi
-    rm ${TMPFILE}
-done
 
+    VERSION=$(echo ${DVERSION} | sed -e 's/^v//g')
+    echo "VERSION: ${VERSION}"
+
+    GITLSFILE=$(mktemp)
+    git ls-remote ${FPATH}.git > ${GITLSFILE}
+
+    TAG="${NAME}/${DVERSION}"
+    LINE=$(grep -P ".*\srefs/tags/${TAG}$" ${GITLSFILE})
+    if [ $? -ne 0 ]; then
+        LINE=$(grep -P ".*\srefs/heads/${TAG}$" ${GITLSFILE})
+    fi
+    WORDS=$(echo ${LINE} | wc -w)
+    if [ ${WORDS} -lt 2 ]; then
+        TAG="${DVERSION}"
+        LINE=$(grep -P ".*\srefs/tags/${TAG}$" ${GITLSFILE})
+        if [ $? -ne 0 ]; then
+            LINE=$(grep -P ".*\srefs/heads/${TAG}$" ${GITLSFILE})
+        fi
+        WORDS=$(echo ${LINE} | wc -w)
+        if [ ${WORDS} -lt 2 ]; then
+            echo "Cannot find ${DVERSION} tag:"
+            cat ${GITLSFILE}
+            echo "${DVERSION}" | grep -P "^v0\.0\.0\-\d{14}\-[0-9a-f]{12}$"
+            if [ $? -eq 0 ]; then
+                LINE=$(grep -P ".*\sHEAD$" ${GITLSFILE})
+                WORDS=$(echo ${LINE} | wc -w)
+                if [ ${WORDS} -lt 2 ]; then
+                    echo "Cannot find HEAD"
+                    exit 1
+                elif [ ${WORDS} -gt 2 ]; then
+                    echo "Found too many entries:"
+                    echo ${WORDS}
+                    exit 1
+                else
+                    VERSION=""
+                    TAG=""
+                    COMMIT=$(echo ${LINE} | awk '{print $1}')
+                fi
+            else
+                echo "Invalid VERSION: ${DVERSION}"
+                exit 1
+            fi
+        elif [ ${WORDS} -gt 2 ]; then
+            echo "Found too many entries:"
+            echo ${WORDS}
+            exit 1
+        else
+            TAG=""
+            COMMIT=""
+        fi
+    elif [ ${WORDS} -gt 2 ]; then
+        echo "Found too many entries:"
+        echo ${WORDS}
+        exit 1
+    else
+        VERSION=""
+        COMMIT=$(echo ${LINE} | awk '{print $1}')
+    fi
+    echo "TAG: ${TAG}"
+    echo "COMMIT: ${COMMIT}"
+
+    LINE="${DMODPATH},${FPATH},,,${VERSION},${TAG},${COMMIT},,,${DTYPE}"
+    echo -n "ENTRY: "
+    echo "${LINE}" | tee -a goipaths.txt
+
+    rm ${PKGGODEVFILE} ${GITLSFILE}
+    echo "=========== END ${IPATH} ==========="
+done
