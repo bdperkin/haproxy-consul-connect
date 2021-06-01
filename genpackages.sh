db5871391cac19be5d371e196eee80a4aae1f3e4
Brandon Perkins <bperkins@redhat.com>
2021-06-01 17:56:41 -0400

    dig deeper for missing packages

--- genpackages.sh
+++ genpackages.sh
@@ -6,3 +6,11 @@ for i in $(grep '^No matching package to install: ' build.log  | cut -d\( -f2 | 
         echo ${i} >> packages.txt
     fi
 done
+
+CHAINROOT=$(grep -P -o "/var/tmp/mock-chain-root-\d+-.+/results/fedora-rawhide-x86_64$" build.log)
+for i in $(grep ': cannot find package "' ${CHAINROOT}/*/build.log | cut -d\" -f2); do
+    grep ^${i}$ packages.txt
+    if [ $? -ne 0 ]; then
+        echo ${i} >> packages.txt
+    fi
+done
