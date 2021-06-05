b4eb1e19143fb668be23775aef2d2eebf5c69069
Brandon Perkins <bperkins@redhat.com>
2021-06-04 23:17:33 -0400

    significant cleanup and simplification

--- gengoipaths.sh
+++ gengoipaths.sh
@@ -3,177 +3,88 @@
 echo "#IPATH,FORGE,SUBDIR,ALTIPATHS,VERSION,TAG,COMMIT,SRCTGZ,DSTTGZ,TYPE" > goipaths.txt
 echo "#IPATH,SUMMARY,DESCRIPTION" > goipaths-docs.txt
 
-for IPATH in $(cat packages.txt); do
-    echo "========== BEGIN ${IPATH} =========="
-    echo "IPATH: ${IPATH}"
+for MOD in $(cat packages.txt); do
+    echo "========== BEGIN ${MOD} =========="
 
-    INAME=$(basename $(echo "${IPATH}" | sed -e 's/\/v[0-9]\+$//g'))
-    echo "INAME: ${INAME}"
+    GOIMPORT=$(curl -s "https://${MOD}?go-get=1" | grep '<meta ' | grep ' name="go-import"' | grep -P -o ' content=".*"' | cut -d\" -f2)
+    if [ "${GOIMPORT}" == "" ]; then
+        GOIMPORT=$(curl -s "http://${MOD}?go-get=1" | grep '<meta ' | grep ' name="go-import"' | grep -P -o ' content=".*"' | cut -d\" -f2)
+        if [ "${GOIMPORT}" == "" ]; then
+            GOIMPORT="${MOD} git https://${MOD}.git"
+        fi
+    fi
+
+    ROOTPATH=$(echo ${GOIMPORT} | awk '{print $1}')
+    VCS=$(echo ${GOIMPORT} | awk '{print $2}')
+    REPOURL=$(echo ${GOIMPORT} | awk '{print $3}' | sed -e 's/\.git//g')
 
     PKGGODEVFILE=$(mktemp)
-    URL="https://pkg.go.dev/${IPATH}"
-    echo "URL: ${URL}"
+    PKGGODEVURL="https://pkg.go.dev/${ROOTPATH}"
+    curl -s "${PKGGODEVURL}" > ${PKGGODEVFILE}
 
-    curl -s ${URL} > ${PKGGODEVFILE}
-    FPATH=$(grep -A 7 Repository ${PKGGODEVFILE} | grep -P -o '<a href=".*" ' | cut -d\" -f2 | grep -P -o "https:\/\/.*")
+    FORGE=$(grep -A 7 Repository ${PKGGODEVFILE} | grep -P -o '<a href=".*" ' | cut -d\" -f2 | grep -P -o "https:\/\/.*")
     DVERSION=$(grep -P 'data-version=".*"' ${PKGGODEVFILE} | head -1 | cut -d\" -f2 | sed -e 's/\+incompatible//g')
     DMODPATH=$(grep -P 'data-mpath=".*"' ${PKGGODEVFILE} | head -1 | cut -d\" -f2)
     DPKGPATH=$(grep -P 'data-ppath=".*"' ${PKGGODEVFILE} | head -1 | cut -d\" -f2)
     DTYPE=$(grep -P 'data-pagetype=".*"' ${PKGGODEVFILE} | head -1 | cut -d\" -f2)
-    if [ "${FPATH}" == "" ]; then
-        echo "Cannot find FPATH"
-        FPATH="https://${IPATH}"
-        DMODPATH=${IPATH}
-        DPKGPATH=${IPATH}
-    fi
+
+    ESCDMODPATH=$(echo "${DMODPATH}" | sed -e 's/\//\\\//g' | sed -e 's/\./\\\./g')
+    SUBDIR=$(echo ${DPKGPATH} | sed -e "s/^${ESCDMODPATH}//g" | sed -e 's/^\///g')
+    FORGEALTIPATH=$(echo ${FORGE} | sed -e 's/^http:\/\///g' | sed -e 's/^https:\/\///g')
+    REPOALTIPATH=$(echo ${REPOURL} | sed -e 's/^http:\/\///g' | sed -e 's/^https:\/\///g')
+    VERSION=$(basename ${DVERSION} | sed -e 's/^v//g')
+    TAG=${DVERSION}
+    IPATH=${DPKGPATH}
     ALTIPATHS=""
-    ALTIPATH=$(echo "${FPATH}" | sed -e 's/^https:\/\///g')
-    if [ "${ALTIPATH}" != "${DMODPATH}" ]; then
-        ALTIPATHS=${ALTIPATH}
+    TYPE=${DTYPE}
+
+    if [ "${FORGEALTIPATH}" != "${IPATH}" ]; then
+        ALTIPATHS+=" ${FORGEALTIPATH}"
     fi
-    FNAME=$(basename $(echo "${FPATH}" | sed -e 's/\/v[0-9]\+$//g'))
-    MNAME=$(basename $(echo "${DMODPATH}" | sed -e 's/\/v[0-9]\+$//g'))
-    PNAME=$(basename $(echo "${DPKGPATH}" | sed -e 's/\/v[0-9]\+$//g'))
-    for D in FPATH FNAME DVERSION DMODPATH MNAME DPKGPATH PNAME DTYPE; do
+    if [ "${REPOALTIPATH}" != "${IPATH}" -a "${FORGEALTIPATH}" != "${REPOALTIPATH}" ]; then
+        ALTIPATHS+=" ${REPOALTIPATH}"
+    fi
+    ALTIPATHS=$(echo ${ALTIPATHS} | sed -e 's/^ //g')
+
+    GITFILE=$(mktemp)
+    git ls-remote ${FORGE} > ${GITFILE}
+    GITREFS=$(grep "/${DVERSION}$" ${GITFILE})
+    GITENTS=$(echo ${GITREFS} | wc -w)
+    if [ ${GITENTS} -lt 2 ]; then
+        GITREFS=$(grep -P "\tHEAD$" ${GITFILE})
+    elif [ ${GITENTS} -gt 2 ]; then
+        echo "TOO MANY VERSIONS:"
+        echo "${GITREFS}"
+        exit 1
+    fi
+    GITENTS=$(echo ${GITREFS} | wc -w)
+    if [ ${GITENTS} -ne 2 ]; then
+        echo "INVALID VERSION:"
+        cat ${GITFILE}
+        exit 1
+    fi
+    COMMIT=$(echo ${GITREFS} | awk '{print $1}')
+    TAGREF=$(echo ${GITREFS} | awk '{print $2}')
+
+    for D in MOD GOIMPORT ROOTPATH VCS REPOURL PKGGODEVURL DVERSION DMODPATH DPKGPATH DTYPE SUBDIR FORGE IPATH ALTIPATHS VERSION TAG COMMIT TYPE; do
         echo "${D}: ${!D}"
     done
 
-    grep "^${DMODPATH}," goipaths.txt
-    if [ $? -eq 0 ]; then
-        echo -n "EXISTS: "
-        grep "^${DMODPATH}," goipaths.txt
-        continue
+    if [ "${TAG}" == "v${VERSION}" ]; then
+        TAG=""
+        COMMIT=""
     fi
 
-    grep "^${DMODPATH}," goipaths-overrides.txt
-    if [ $? -eq 0 ]; then
-        echo -n "OVERRIDE: "
-        LINE=$(grep "^${DMODPATH}," goipaths-overrides.txt)
-        echo "${LINE}" | tee -a goipaths.txt
-        continue
-    fi
-
-    VERSION=$(echo ${DVERSION} | sed -e 's/^v//g')
-    echo "VERSION: ${VERSION}"
-
-    GITLSFILE=$(mktemp)
-    git ls-remote ${FPATH}.git > ${GITLSFILE}
-    if [ $? -ne 0 ]; then
-        echo "Problem retrieving remote list of ${FPATH}.git"
-        continue
-    fi
-
-    TAG="${INAME}/${DVERSION}"
-    LINE=$(grep -P ".*\srefs/tags/${TAG}$" ${GITLSFILE})
-    if [ $? -ne 0 ]; then
-        LINE=$(grep -P ".*\srefs/heads/${TAG}$" ${GITLSFILE})
-    fi
-    WORDS=$(echo ${LINE} | wc -w)
-    if [ ${WORDS} -lt 2 ]; then
-        TAG="${FNAME}/${DVERSION}"
-        LINE=$(grep -P ".*\srefs/tags/${TAG}$" ${GITLSFILE})
-        if [ $? -ne 0 ]; then
-            LINE=$(grep -P ".*\srefs/heads/${TAG}$" ${GITLSFILE})
-        fi
-        WORDS=$(echo ${LINE} | wc -w)
-        if [ ${WORDS} -lt 2 ]; then
-            TAG="${MNAME}/${DVERSION}"
-            LINE=$(grep -P ".*\srefs/tags/${TAG}$" ${GITLSFILE})
-            if [ $? -ne 0 ]; then
-                LINE=$(grep -P ".*\srefs/heads/${TAG}$" ${GITLSFILE})
-            fi
-            WORDS=$(echo ${LINE} | wc -w)
-            if [ ${WORDS} -lt 2 ]; then
-                TAG="${PNAME}/${DVERSION}"
-                LINE=$(grep -P ".*\srefs/tags/${TAG}$" ${GITLSFILE})
-                if [ $? -ne 0 ]; then
-                    LINE=$(grep -P ".*\srefs/heads/${TAG}$" ${GITLSFILE})
-                fi
-                WORDS=$(echo ${LINE} | wc -w)
-                if [ ${WORDS} -lt 2 ]; then
-                    TAG="${DVERSION}"
-                    LINE=$(grep -P ".*\srefs/tags/${TAG}$" ${GITLSFILE})
-                    if [ $? -ne 0 ]; then
-                        LINE=$(grep -P ".*\srefs/heads/${TAG}$" ${GITLSFILE})
-                    fi
-                    WORDS=$(echo ${LINE} | wc -w)
-                    if [ ${WORDS} -lt 2 ]; then
-                        echo "Cannot find ${DVERSION} tag:"
-                        cat ${GITLSFILE}
-                        echo "${DVERSION}" | grep -P "^v0\.0\.0\-\d{14}\-[0-9a-f]{12}$"
-                        if [ $? -eq 0 ]; then
-                            LINE=$(grep -P ".*\sHEAD$" ${GITLSFILE})
-                            WORDS=$(echo ${LINE} | wc -w)
-                            if [ ${WORDS} -lt 2 ]; then
-                                echo "Cannot find HEAD"
-                                exit 1
-                            elif [ ${WORDS} -gt 2 ]; then
-                                echo "Found too many entries:"
-                                echo ${WORDS}
-                                exit 1
-                            else
-                                VERSION=""
-                                TAG=""
-                                COMMIT=$(echo ${LINE} | awk '{print $1}')
-                            fi
-                        else
-                            echo "Invalid VERSION: ${DVERSION}"
-                            echo "Invalid TAG: ${TAG}"
-                            exit 1
-                        fi
-                    elif [ ${WORDS} -gt 2 ]; then
-                        echo "Found too many entries:"
-                        echo ${WORDS}
-                        exit 1
-                    else
-                        TAG=""
-                        COMMIT=""
-                    fi
-                elif [ ${WORDS} -gt 2 ]; then
-                    echo "Found too many entries:"
-                    echo ${WORDS}
-                    exit 1
-                else
-                    VERSION=""
-                    COMMIT=$(echo ${LINE} | awk '{print $1}')
-                fi
-            elif [ ${WORDS} -gt 2 ]; then
-                echo "Found too many entries:"
-                echo ${WORDS}
-                exit 1
-            else
-                VERSION=""
-                COMMIT=$(echo ${LINE} | awk '{print $1}')
-            fi
-        elif [ ${WORDS} -gt 2 ]; then
-            echo "Found too many entries:"
-            echo ${WORDS}
-            exit 1
-        else
-            VERSION=""
-            COMMIT=$(echo ${LINE} | awk '{print $1}')
-        fi
-    elif [ ${WORDS} -gt 2 ]; then
-        echo "Found too many entries:"
-        echo ${WORDS}
-        exit 1
-    else
-        VERSION=""
-        COMMIT=$(echo ${LINE} | awk '{print $1}')
-    fi
-    echo "TAG: ${TAG}"
-    echo "COMMIT: ${COMMIT}"
-
-    LINE="${DMODPATH},${FPATH},,${ALTIPATHS},${VERSION},${TAG},${COMMIT},,,${DTYPE}"
+    LINE="${IPATH},${FORGE},${SUBDIR},${ALTIPATHS},${VERSION},${TAG},${COMMIT},${SRCTGZ},${DSTTGZ},${TYPE}"
     echo -n "ENTRY: "
     echo "${LINE}" | tee -a goipaths.txt
 
     DESCRIPTION=$(cat ${PKGGODEVFILE} | grep -A 1000 'Documentation-overviewHeader' | grep -m 1 -B 100 '^</p>'  | grep -A 100 '^<p>' | tidy 2>/dev/null | lynx -dump -nolist -stdin | sed -e 's/^   //g' | tr '\n' ' ' | sed -e 's/,/\&#44;/g')
     SUMMARY=$(echo ${DESCRIPTION} | cut -d\. -f1)
-    DOCS="${DMODPATH},${SUMMARY},${DESCRIPTION}"
+    DOCS="${IPATH},${SUMMARY},${DESCRIPTION}"
     echo -n "DOCS: "
     echo "${DOCS}" | tee -a goipaths-docs.txt
 
-    rm ${PKGGODEVFILE} ${GITLSFILE}
-    echo "=========== END ${IPATH} ==========="
+    rm ${PKGGODEVFILE} ${GITFILE}
+    echo "=========== END ${IMPORT} ==========="
 done
