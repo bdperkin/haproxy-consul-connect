0c7ee279ade0e04afcca0fb4fe26859c18030617
Brandon Perkins <bperkins@redhat.com>
2021-06-08 09:53:50 -0400

    disable checking for missing packages in build

--- genpackages.sh
+++ genpackages.sh
@@ -9,10 +9,10 @@ for i in $(grep '^No matching package to install: ' build.log  | cut -d\( -f2 | 
     fi
 done
 
-CHAINROOT=$(grep -P -o "/var/tmp/mock-chain-root-\d+-.+/results/fedora-rawhide-x86_64$" build.log)
-for i in $(grep ': cannot find package "' ${CHAINROOT}/*/build.log | cut -d\" -f2); do
-    grep ^${i}$ packages.txt
-    if [ $? -ne 0 ]; then
-        echo ${i} >> packages.txt
-    fi
-done
+#CHAINROOT=$(grep -P -o "/var/tmp/mock-chain-root-\d+-.+/results/fedora-rawhide-x86_64$" build.log)
+#for i in $(grep ': cannot find package "' ${CHAINROOT}/*/build.log | cut -d\" -f2); do
+#    grep ^${i}$ packages.txt
+#    if [ $? -ne 0 ]; then
+#        echo ${i} >> packages.txt
+#    fi
+#done
