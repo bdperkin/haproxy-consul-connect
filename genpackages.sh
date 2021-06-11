0d25b0ea91457355f84fbcb628c57a725948f177
Brandon Perkins <bperkins@redhat.com>
2021-06-11 17:17:27 -0400

    pass 1

--- genpackages.sh
+++ genpackages.sh
@@ -10,9 +10,11 @@ for i in $(grep -P -o 'golang\(.*\)' build.log | sed -e 's/^golang(//g' | sed -e
 done
 
 CHAINROOT=$(grep -P -o "/var/tmp/mock-chain-root-\d+-.+/results/fedora-rawhide-x86_64$" build.log | uniq)
-for i in $(grep ': cannot find package "' $(ls -1 ${CHAINROOT}/*/build.log) | cut -d\" -f2); do
-    grep ^${i}$ packages.txt
-    if [ $? -ne 0 ]; then
-        echo ${i} >> packages.txt
-    fi
-done
+if [ "${CHAINROOT}" != "" ]; then
+    for i in $(grep ': cannot find package "' $(ls -1 ${CHAINROOT}/*/build.log) | cut -d\" -f2); do
+        grep ^${i}$ packages.txt
+        if [ $? -ne 0 ]; then
+            echo ${i} >> packages.txt
+        fi
+    done
+fi
